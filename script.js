5169c75702d3cbff180a792d546f74d07c04dd13
Jones23 <quarteyjonie18@gmail.com>
2019-12-18 14:19:18 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let btnSubmit = document.getElementById('submit');
+
+
+ btnSubmit.addEventListener('click', () => btnSubmit.style.background = 'green');
+ btnSubmit.addEventListener('click', () => btnSubmit.textContent = 'Successful');
